7b30c07ba0ddf81c88aab341a44122b0557e73a2
unknown <agussyabany@gmail.com>
2023-10-30 17:11:42 +0800

    guards problem

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable implements JWTSubject
      *
      * @var array<int, string>
      */
+    protected $guard = 'api';
     protected $fillable = [
         'name',
         'email',
