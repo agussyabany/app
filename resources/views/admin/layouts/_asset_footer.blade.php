5e31544c33f90f5e7dfe47e19f341fee48a5b281
unknown <agussyabany@gmail.com>
2023-11-24 15:07:36 +0800

    Menambhakan js library Select2

--- resources/views/admin/layouts/_asset_footer.blade.php
+++ resources/views/admin/layouts/_asset_footer.blade.php
@@ -5,4 +5,5 @@
 {{-- <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script> --}}
 <script src="{{ asset('assets/boots/js/myjs/menu.js')}}"></script>
 <script src="{{ asset('assets/boots/js/myjs/funct.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
