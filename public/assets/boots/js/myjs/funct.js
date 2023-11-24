0393de3c525273bd09d169e16dd6c807972e965f
unknown <agussyabany@gmail.com>
2023-11-24 15:06:00 +0800

    membuat refresh tabel DIVSI dan RUANGAN

--- public/assets/boots/js/myjs/funct.js
+++ public/assets/boots/js/myjs/funct.js
@@ -39,3 +39,44 @@ function refDep()
          });
         })
 }
+
+function refDiv()
+{
+    $.get('/divisi', function(data) {
+        var i = 0;
+        var table = $("#tbl_div").DataTable();
+        table.clear().draw();
+        $('#modal_body').html('');
+        $.each(data.data, function(index, item) {
+            var editButton = '<a class="btn btn-sm btn-warning edit-div" data-id="' + item.id + '" href="#"><i class="fa-solid fa-edit"></i></a>';
+            var deleteButton = '<a class="btn btn-sm btn-danger delete-div" data-id="' + item.id + '" href="#"><i class="fa-solid fa-trash"></i></a>';
+            table.row.add([
+                 ++i,
+                item.nama_div,
+                item.kode_div,
+                item.id ? editButton + '' + deleteButton : ''
+            ]).draw();
+         });
+        })
+}
+
+function refRuang()
+{
+    $.get('/ruang', function(data) {
+        var i = 0;
+        var table = $("#tbl_ruang").DataTable();
+        table.clear().draw();
+        $('#modal_body').html('');
+        $.each(data.data, function(index, item) {
+            var editButton = '<a class="btn btn-sm btn-warning edit" data-id="' + item.id + '" href="#"><i class="fa-solid fa-edit"></i></a>';
+            var deleteButton = '<a class="btn btn-sm btn-danger delete" data-id="' + item.id + '" href="#"><i class="fa-solid fa-trash"></i></a>';
+            table.row.add([
+                 ++i,
+                item.nama_ruang,
+                item.kode,
+                item.id ? editButton + '' + deleteButton : ''
+            ]).draw();
+         });
+        })
+}
+
