d8e8123f57ce9f73161a7279041305adf2fa54b6
unknown <agussyabany@gmail.com>
2024-01-20 06:19:05 +0800

    inisialisasi diklat

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,44 +21,44 @@ class UserSeeder extends Seeder
         // ]);
         // $admin->assignRole('admin');
 
-        $aset = User::create(
-            // [
-            //     'name' => 'AAM ROBIDIN NOOR, S.E.',
-            //     'email'=>'robi@aset.smd',
-            //     'nip' =>'1978.2002.1.370',
-            //     'jabat'=>2,
-            //     'divisi'=>27,
-            //     'img'=>'robi.jpg',
-            //     'password'=>bcrypt('robi2024')
-            // ]
-            // [
-            //     'name' => 'MUHAMMAD AGUS SYABANY',
-            //     'email'=>'agus@aset.smd',
-            //     'nip' =>'1982.2008.1.444',
-            //     'jabat'=>3,
-            //     'divisi'=>22,
-            //     'img'=>'agus.jpg',
-            //     'password'=>bcrypt('a')
-            // ],
-            [
-                'name' => 'DEA INRUM RISTYA',
-                'email'=>'dea@aset.smd',
-                'nip' =>'1991.2020.2.694',
-                'jabat'=>3,
-                'divisi'=>27,
-                'img'=>'dea.jpg',
-                'password'=>bcrypt('dea2024')
-            ]
-        );
-        $aset->assignRole('aset');
+        // $aset = User::create(
+        //     // [
+        //     //     'name' => 'AAM ROBIDIN NOOR, S.E.',
+        //     //     'email'=>'robi@aset.smd',
+        //     //     'nip' =>'1978.2002.1.370',
+        //     //     'jabat'=>2,
+        //     //     'divisi'=>27,
+        //     //     'img'=>'robi.jpg',
+        //     //     'password'=>bcrypt('robi2024')
+        //     // ]
+        //     // [
+        //     //     'name' => 'MUHAMMAD AGUS SYABANY',
+        //     //     'email'=>'agus@aset.smd',
+        //     //     'nip' =>'1982.2008.1.444',
+        //     //     'jabat'=>3,
+        //     //     'divisi'=>22,
+        //     //     'img'=>'agus.jpg',
+        //     //     'password'=>bcrypt('a')
+        //     // ],
+        //     [
+        //         'name' => 'DEA INRUM RISTYA',
+        //         'email'=>'dea@aset.smd',
+        //         'nip' =>'1991.2020.2.694',
+        //         'jabat'=>3,
+        //         'divisi'=>27,
+        //         'img'=>'dea.jpg',
+        //         'password'=>bcrypt('dea2024')
+        //     ]
+        // );
+        // $aset->assignRole('aset');
 
-        // $diklat = User::create([
-        //     'name' => 'diklat',
-        //     'email'=>'diklat@gmail.com',
-        //     'password'=>bcrypt('12345678')
+        $diklat = User::create([
+            'name' => 'Muhammad Agus Syabany',
+            'email'=>'agus@diklat.smd',
+            'password'=>bcrypt('a')
 
-        // ]);
-        // $diklat->assignRole('diklat');
+        ]);
+        $diklat->assignRole('diklat');
 
         // $api = User::create([
         //     'name' => 'api',
---
+++ resources/views/diklat/layouts/_asset_header.blade.php
@@ -0,0 +1,36 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="{{asset('assets/boots/css/mystyle.css')}}">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap" rel="stylesheet">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<style>
+    /* Add your custom styles here */
+    .hoverable {
+      transition: background-color 0.3s ease;
+    }
+
+    .hoverable:hover {
+      background-color: #565e66; /* Change to your desired hover background color */
+      color: white;
+    }
+    body{
+      font-family: 'EB Garamond', serif;
+    }
+
+    .nav{
+      font-family: 'EB Garamond', serif;
+    }
+
+    .kop{
+      font-family: 'EB Garamond', serif;
+    }
+    
+  </style>
+
