93cb465d0825593dc192426a4c0b549ebd2efb1e
Alamsyah0041 <108207169+Alamsyah0041@users.noreply.github.com>
2024-09-20 10:33:18 +0800

    asset footer

--- routes/web.php
+++ routes/web.php
@@ -249,7 +249,6 @@ Route::middleware('auth','verified','role:diklat')->group(function () {
     Route::get('/', function () {
         return redirect('/diklat.dashboard');
     });
-<<<<<<< HEAD
     Route::get('/diklat.dashboard',[Diklatcontroller::class, 'index']);
     Route::get('/pegawai',[PegawaiController::class, 'pegawai'])->name('pegawai.index');
     Route::get('/pemateri',[PemateriController::class, 'pemateri']);
@@ -264,7 +263,6 @@ Route::middleware('auth','verified','role:diklat')->group(function () {
 
 
 
-=======
     Route::get('/diklat.dashboard', [DiklatController::class, 'index'])->name('divisi.index');
     Route::post('/divisi', [DivisiController::class, 'store'])->name('divisi.store');
     Route::get('/divisi', [DivisiController::class, 'index']);
@@ -281,7 +279,6 @@ Route::put('/bagian/{id}', [DivisiDiklatController::class, 'update'])->name('bag
 
 
    Route::get('divisidiklat',[DivisiDiklatController::class, 'index']);
->>>>>>> 687e8359e353a48f9554105fd17b64f2bc1eb285
 });
 
 Route::middleware('auth','verified','role:soc')->group(function () {
